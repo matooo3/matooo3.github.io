13d65b23aecefd34c908c586b86557e6185ad8bf
Matthias Vetter <matze2948@gmail.com>
2023-11-09 19:54:34 +0100

    Changed nav-bar design & added functions openNav() and closeNav() to open and close nav-bar

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.querySelector(".invisible").style.display = "block"; /*Nav wird sichtbar gemacht*/
+}
+
+function closeNav() {
+    document.querySelector(".invisible").style.display = "none"; /*Nav wird versteckt*/
+}
