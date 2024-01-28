bd33e0eb2ecb8550ec894ff942a46eb5b8edb5e4
Matthias Vetter <matze2948@gmail.com>
2024-01-28 18:56:00 +0100

    service worker added

---
+++ serviceworker.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.error('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
