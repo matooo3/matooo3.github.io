4ea2e6aadc8dd72ba5689869a362ce36b8a90f28
Matthias Vetter <matze2948@gmail.com>
2024-01-30 20:17:09 +0100

    added menu button function + animated right left button

--- connect/script.js
+++ connect/script.js
@@ -18,4 +18,68 @@ function openLink(link) {
     window.location.href = link;
 }
 
+navbarVisivility = false;
+
+function toggleVisibility() {
+    if(navbarVisivility) {
+        deleteVisibility();
+    } else {
+        addVisibility();
+    }
+}
+
+function addVisibility() {
+    const navElements = document.querySelectorAll('.invisible');
+
+    navElements.forEach((el) => {
+        el.classList.add('visible')
+    })
+    navElements.forEach((el) => {
+        el.classList.remove('invisible')
+    })
+
+    navbarVisivility = true;
+}
+
+function deleteVisibility() {
+    const navElements = document.querySelectorAll('.visible');
+
+    navElements.forEach((el) => {
+        el.classList.add('invisible')
+    })
+    navElements.forEach((el) => {
+        el.classList.remove('visible')
+    })
+
+    navbarVisivility = false;
+}
+
+//  BUTTON ------------------------------
+var btns = document.querySelectorAll('.btn');
+var paginationWrapper = document.querySelector('.pagination-wrapper');
+var bigDotContainer = document.querySelector('.big-dot-container');
+var littleDot = document.querySelector('.little-dot');
+
+for(var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', btnClick);
+  }
+  
+  function btnClick() {
+    if(this.classList.contains('btn--prev')) {
+      paginationWrapper.classList.add('transition-prev');
+    } else {
+      paginationWrapper.classList.add('transition-next');  
+    }
+    
+    var timeout = setTimeout(cleanClasses, 500);
+  }
+  
+  function cleanClasses() {
+    if(paginationWrapper.classList.contains('transition-next')) {
+      paginationWrapper.classList.remove('transition-next')
+    } else if(paginationWrapper.classList.contains('transition-prev')) {
+      paginationWrapper.classList.remove('transition-prev')
+    }
+  }
+
 
